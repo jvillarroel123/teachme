c9856dd1b46c6093fb16b786107e7856c8dcd94b
jvillarroel123 <javier.villarroel.oyarzun@gmail.com>
2016-05-21 19:48:49 -0400

    Modificacion a la tabla de votos y a la forma de hacer seed

--- database/migrations/2016_05_21_230920_create_ticket_votes_table.php
+++ database/migrations/2016_05_21_230920_create_ticket_votes_table.php
@@ -16,10 +16,12 @@ class CreateTicketVotesTable extends Migration
             $table->increments('id');
 
             $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')
+                    ->on('users')->onDelete('cascade');
 
             $table->unsignedInteger('ticket_id');
-            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('ticket_id')->references('id')
+                    ->on('tickets')->onDelete('cascade');
 
             $table->timestamps();
         });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,34 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->truncateTables(array(
+            'users',
+            'password_resets',
+            'tickets',
+            'ticket_votes',
+            'ticket_comments'
+        ));
+
         $this->call(UserTableSeeder::class);
 
         Model::reguard();
+    }
+
+    private function truncateTables(array $tables)
+    {
+        //se borran las claves foraneas para poder limpiar las tablas
+        $this->checkForeignKeys(false);
+
+        foreach ($tables as $table) {
+            \DB::table($table)->truncate();
+        }
+        //las activamos para cargar correctamente los seeders
+        $this->checkForeignKeys(true);
+    }
+
+    private function checkForeignKeys($check)
+    {
+        $check = $check ? '1' : '0';
+        \DB::statement("SET FOREIGN_KEY_CHECKS = $check;");
     }
 }
