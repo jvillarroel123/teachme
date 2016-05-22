55c31370956e689ebaa61b474561f1e27be8e6a6
jvillarroel123 <javier.villarroel.oyarzun@gmail.com>
2016-05-22 19:51:08 -0400

    Se agregaron rutas y se asociaron al nuevo controlador TicketsController

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TeachMe\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use TeachMe\Http\Requests;
+use TeachMe\Http\Controllers\Controller;
+
+class TicketsController extends Controller
+{
+    //
+    public function latest()
+    {
+    	dd('latest')
+    }
+
+    public function popular()
+    {
+
+    }
+
+	public function open()
+    {
+
+    }
+
+    public function closed()
+    {
+
+    }
+
+    public function details($id)
+    {
+
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,9 +10,33 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-use TeachMe\Entities\User;
 
-Route::get('/', function () {
-	dd(User::all()->random()->id);
-    return view('welcome');
-});
+Route::get('/', [
+	'as' => 'tickets.latest',
+	'uses' => 'TicketsController@latest'
+]);
+
+Route::get('/populares', [
+	'as' => 'tickets.popular',
+	'uses' => 'TicketsController@popular'
+]);
+
+Route::get('/pendientes', [
+	'as' => 'tickets.open',
+	'uses' => 'TicketsController@open'
+]);
+
+Route::get('/tutoriales', [
+	'as' => 'tickets.closed',
+	'uses' => 'TicketsController@closed'
+]);
+
+Route::get('/solicitud/{id}',[
+	'as' => 'tickets.details',
+	'uses' => 'TicketsController@details'
+]);
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController'
+]);
